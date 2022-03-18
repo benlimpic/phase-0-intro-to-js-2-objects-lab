a08d151bdcd60ce2732a13911f95b3621c5259dd
init.defaultBranch <benlimpic@gmail.com>
2022-03-17 20:41:40 -0700

    commit tree 51f6c211a07b8fb5a8acbab0fb5fe57c8e926a4b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+  name: "Ben",
+  streetAddress: "1234 number st",
+};
+
+const updateEmployeeWithKeyAndValue = (obj, key, value) => {
+  return { ...obj, [key]: value };
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const clone = { ...employee };
+  delete clone[key];
+  return clone;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
+deleteFromEmployeeBy;
